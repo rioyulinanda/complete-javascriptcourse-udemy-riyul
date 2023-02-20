150e4d758d9fea304c59fa3958b2e1f65efbe33a
Rio Y Kurniawan <rioyulinanda@gmail.com>
2023-02-20 20:42:43 +0800

    first push

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Rio");
+console.log(23);
+
+let firstName = "Shella";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let rio_shella = "RS";
+let $function = 27;
+
+let person = "rio";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
